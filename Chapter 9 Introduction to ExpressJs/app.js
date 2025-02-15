53d88ceb2dc0dce83f3ca320c3b093026944c09c
JatinbVyas <vyassjatin@gmail.com>
2025-02-15 20:00:00 +0530

    define middleware using express object and hadles req and res

--- Chapter 9 Introduction to ExpressJs/app.js
+++ Chapter 9 Introduction to ExpressJs/app.js
@@ -10,6 +10,21 @@ const requestHandler = require('./fileSquence');
 //Execute expressJs
 const appExpressIntro = express();
 
+/*appExpressIntro.Use methos used for register a middleware and handle the req and res.
+  Earlier same as requesthandler. In simple Node this was used to write in createServer method but
+  here in ExpressJs req and res handles in middleware using Use method.
+  next() => this function use to move cursor or execution pointer to next immediate registerd middleware.
+  here is case second middleware.
+*/
+appExpressIntro.use((req, res, next) => {
+  console.log('This is first middleware. ', req.url, req.method);
+  next();
+});
+
+appExpressIntro.use((req, res, next) => {
+  console.log('This is second middleware. ', req.url, req.method);
+});
+
 const server = http.createServer(appExpressIntro);
 
 const port = 3000;
