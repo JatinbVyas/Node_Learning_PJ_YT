248054642626e4ab011085c6916b8ad2b71bb78c
JatinbVyas <vyassjatin@gmail.com>
2025-02-19 00:32:29 +0530

    added nodemon config, also written and fetch data from file now, seen the use of callback

--- Chapter 13 MVC/controllers/homes.js
+++ Chapter 13 MVC/controllers/homes.js
@@ -1,4 +1,6 @@
-const registeredHomes = [];
+
+//Local module
+const HomeClass = require('../models/homeModel');
 
 exports.getAddhome = (req, res, next) => {
   res.render('addHome',{pageTitle:"Add your home", currentPage:"addHome"});
@@ -6,11 +8,31 @@ exports.getAddhome = (req, res, next) => {
 
 exports.postAddhome = (req, res, next) => {
   console.log(req.body);
-  registeredHomes.push(req.body);
+  
+  /**
+   * Now instead of array we will use of new class
+   */
+  const home = new HomeClass(
+    req.body.houseName,
+    req.body.price,
+    req.body.location,
+    req.body.rating,
+    req.body.photoUrl
+  );
+
+  home.saveHome();
+
   res.render('homeAdded',{pageTitle:"home added succesfully",currentPage:"homeAdded"});
 };
 
 exports.getHomes = (req, res, next) => {
-  console.log(registeredHomes);
-  res.render('home',{registeredHomes: registeredHomes, pageTitle: "airbnbHome",currentPage:"home"});
+  /**
+   * fetchAll is returning using callback so res.render comes under function becase once read operation
+   * is complete and value get returns it will recevied in variable , here it is registeredHomes and then
+   * we can proceed further.
+   */
+  HomeClass.fetchAll((registeredHomes)=> {
+    res.render('home',{registeredHomes: registeredHomes, pageTitle: "airbnbHome",currentPage:"home"});
+  });
+  
 };
