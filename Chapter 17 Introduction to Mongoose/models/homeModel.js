8a4d6c80e0d99414e46eeac78bd6eac8606dd636
JatinbVyas <vyassjatin@gmail.com>
2025-03-26 21:13:20 +0530

    removed old code of home model

--- Chapter 17 Introduction to Mongoose/models/homeModel.js
+++ Chapter 17 Introduction to Mongoose/models/homeModel.js
@@ -1,59 +1,15 @@
-const { ObjectId } = require("mongodb");
-
-module.exports = class HomeClass {
-  constructor(id, houseName, price, location, rating, photoUrl, description) {
-    this._id = id;
+/**
+ * this._id = id;
     this.houseName = houseName;
     this.price = price;
     this.location = location;
     this.rating = rating;
     this.photoUrl = photoUrl;
     this.description = description;
-  }
 
-  //Define function to save/push home object.
-  saveHome() {
-    console.log("value of id is::", this);
-    if (this._id) {
-      //this is update case.
-      const updateFields = {
-        houseName: this.houseName,
-        price: this.price,
-        location: this.location,
-        rating: this.rating,
-        photoUrl: this.photoUrl,
-        description: this.description,
-      };
-      const db = getdb();
-      return db
-        .collection("homes")
-        .updateOne(
-          { _id: new ObjectId(String(this._id)) },
-          { $set: updateFields }
-        );
-    } else {
-      const db = getdb();
-      return db.collection("homes").insertOne(this);
-    }
-  }
+    saveHome()
+     fetchAll()
+     findHomeByid(homeId)
+     deleteHomeByid(homeId)
 
-  static fetchAll() {
-    const db = getdb();
-    return db.collection("homes").find().toArray();
-  }
-
-  static findHomeByid(homeId) {
-    const db = getdb();
-    return db
-      .collection("homes")
-      .find({ _id: new ObjectId(String(homeId)) })
-      .next();
-  }
-
-  static deleteHomeByid(homeId) {
-    const db = getdb();
-    return db
-      .collection("homes")
-      .deleteOne({ _id: new ObjectId(String(homeId)) });
-  }
-};
+ */
