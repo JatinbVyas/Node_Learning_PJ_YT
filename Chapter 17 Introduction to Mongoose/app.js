8f61b8ebc45207d1286969da11d1162d84f172c6
JatinbVyas <vyassjatin@gmail.com>
2025-03-26 20:49:22 +0530

    connection to mongo db using mongoose

--- Chapter 17 Introduction to Mongoose/app.js
+++ Chapter 17 Introduction to Mongoose/app.js
@@ -1,16 +1,17 @@
 //Core module
-const path = require('path');
+const path = require("path");
 
 //External module
-const express = require('express');
+const express = require("express");
 
 //Internal module
-const storeRouter = require('./routes/storeRouter');
+const storeRouter = require("./routes/storeRouter");
 //Below is destructure the houseRouter because in that file multiple things are exported.
-const {houseRouter} = require('./routes/hosrRouter');
-const rootDir = require('./utils/pathUtil');
-const { NotFoundPage } = require('./controllers/404NoutFound');
-const { mongoConnect } = require('./utils/databaseUtil');
+const { houseRouter } = require("./routes/hosrRouter");
+const rootDir = require("./utils/pathUtil");
+const { NotFoundPage } = require("./controllers/404NoutFound");
+const { mongoConnect } = require("./utils/databaseUtil");
+const { default: mongoose } = require("mongoose");
 
 const appAirbnb = express();
 
@@ -19,8 +20,8 @@ const appAirbnb = express();
  * using this set method, we define that we are using EJS template engine
  * and our view pages are in views folder
  * */
-appAirbnb.set('view engine', 'ejs');
-appAirbnb.set('views', 'views');
+appAirbnb.set("view engine", "ejs");
+appAirbnb.set("views", "views");
 
 //Creating middleware
 appAirbnb.use((req, res, next) => {
@@ -38,23 +39,35 @@ appAirbnb.use(express.urlencoded());
 //Below sysntax use to call router that is created for sepration of code.
 appAirbnb.use(storeRouter);
 
-
-
-appAirbnb.use("/host",houseRouter);
+appAirbnb.use("/host", houseRouter);
 
 /**
  * Below express.static used in middleware to make file public, so that can be used or can be access at client side.
  *  You just need to create public folder and place files inside that folder and add below middleware.
  */
-appAirbnb.use(express.static(path.join(rootDir,"public")));
+appAirbnb.use(express.static(path.join(rootDir, "public")));
 
 appAirbnb.use(NotFoundPage);
 
 //Connect mongo db first and in return success callback start server.
 const PORT = 3000;
-mongoConnect( () => {
-//Now start and listen server.
-appAirbnb.listen(PORT, () => {
-  console.log(`Server is running on address http://localhost:${PORT}`);
-});
-});
+const dbPath =
+  "mongodb+srv://jatinmongo:jatinmongo321$@jackmongo.x0ybk.mongodb.net/airbnbhomes?retryWrites=true&w=majority&appName=JackMongo";
+/**
+ * Here we learning new concept of mongoose.
+ * So earlier we were connecting using mongodb connected now instead of that we are connecting using
+ * mongoose.connect and once it connected we are starting our server.
+ * for connecting we need to pass our db path/url with db name after mogodb.net/ {here db name}?
+ */
+mongoose
+  .connect(dbPath)
+  .then(() => {
+    console.log("Connected to monogoDB");
+    //Now start and listen server.
+    appAirbnb.listen(PORT, () => {
+      console.log(`Server is running on address http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error while connecting to Mongoose. ", err);
+  });
